4c8674d7f259bb1258224e4abce10b0b961b2df9
Liz Kerber <emkerber@outlook.com>
2016-07-01 00:19:06 -0500

    completed some of Hard Mode

---
+++ README.md
@@ -0,0 +1,8 @@
+This was a solo assignment at Prime Digital Academy, assigned on June 30th of 2016. Below are the instructions.
+
+Solo API Challenge
+
+In this challenge we will be using the OMDb API to create a web page displaying your top 3 movies. You should make 3 queries to the OMDb API for each of your movies and append them to your screen. Each movie entry should use at least 3 keys from the returned data (one being the image). Your page should be styled with CSS and look presentable.
+
+Hard Mode
+Add a search input that will allow you to search for a movie and display the results in a meaningful way.
--- solo_API.js
+++ solo_API.js
@@ -1,41 +1,41 @@
-//var poster = '<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">';
-//var text = '<p>' + data.Title + '</p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>';
-
-// function appendMovie(data, movieNum) {
-//   $('#movie' + movieNum + ' .picture').append('<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">');
-//   $('#movie' + movieNum + ' .text').append('<p>' + data.Title + '</p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
-// };
-//
+//function to be executed if there is an error processing the request
 function errorLog(movieNum) {
   console.log('An error occurred with getting movie number ' + movieNum);
 };
 
+//gets the JSON code for the first movie and appends to the DOM
 $.get('http://www.omdbapi.com/?i=tt2312602&plot=short&r=json')
 .then(function(data) {
   $('#movie1 .picture').append('<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">');
-  $('#movie1 .text').append('<p><b>' + data.Title + '</b></p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
+  $('#movie1 .text').append('<p>' + data.Title + '</p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
 })
 .catch(errorLog(1));
 
+//gets the JSON code for the second movie and appends to the DOM
 $.get('http://www.omdbapi.com/?i=tt0784972&plot=short&r=json')
 .then(function(data) {
   $('#movie2 .picture').append('<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">');
-  $('#movie2 .text').append('<p><b>' + data.Title + '</b></p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
+  $('#movie2 .text').append('<p>' + data.Title + '</p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
 })
 .catch(errorLog(2));
 
+//gets the JSON code for the third movie and appends to the DOM
 $.get('http://www.omdbapi.com/?i=tt1399683&plot=short&r=json')
 .then(function(data) {
   $('#movie3 .picture').append('<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">');
-  $('#movie3 .text').append('<p><b>' + data.Title + '</b></p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
+  $('#movie3 .text').append('<p>' + data.Title + '</p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
 })
 .catch(errorLog(3));
-//function appendMovie(data, movieNum) {
-//  .then(function(data) {
-    // $('#movie' + movieNum + ' .picture').append('<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">');
-  //   // $('#movie' + movieNum + ' .text').append('<p>' + data.Title + '</p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
+
+//listens for a click event on the Submit! button and reads the input in the text bar
+$('#submitButton').on('click', function(event) {
+  event.preventDefault(); //to prevent the page from reloading
+  //$('#searchResult').empty(); //to clear previous searches
+  // var input = $('#searchInput').val(); //assigns the text input to a value
+  // $.get('http://www.omdbapi.com/?s=' + input) //searches the API for the movie
+  // .then(function(data) { //appends movie information to the DOM
+  //   $('#searchResult .picture').append('<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">');
+  //   $('#searchResult .text').append('<p>' + data.Title + '</p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
   // })
-  // .catch(function() {
-  //   console.log('An error occurred with getting movie number ' + movieNum);
-  // })
-//};
+  // .catch(errorLog(4)); //function is executed if there is an error processing the request
+});
