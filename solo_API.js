5647cd6bd9135f352acfe3289acb40001db11d9c
Liz Kerber <emkerber@outlook.com>
2016-06-30 23:23:49 -0500

    completed basic part of challenge

--- solo_API.js
+++ solo_API.js
@@ -1,21 +1,35 @@
-function appendMovie(data, movieNum) {
-  $('#movie' + movieNum + ' .picture').append('<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">');
-  $('#movie' + movieNum + ' .text').append('<p>' + data.Title + '</p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
-};
+//var poster = '<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">';
+//var text = '<p>' + data.Title + '</p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>';
 
+// function appendMovie(data, movieNum) {
+//   $('#movie' + movieNum + ' .picture').append('<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">');
+//   $('#movie' + movieNum + ' .text').append('<p>' + data.Title + '</p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
+// };
+//
 function errorLog(movieNum) {
   console.log('An error occurred with getting movie number ' + movieNum);
 };
 
 $.get('http://www.omdbapi.com/?i=tt2312602&plot=short&r=json')
-.then(appendMovie(1)).catch(errorLog(1));
+.then(function(data) {
+  $('#movie1 .picture').append('<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">');
+  $('#movie1 .text').append('<p><b>' + data.Title + '</b></p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
+})
+.catch(errorLog(1));
 
 $.get('http://www.omdbapi.com/?i=tt0784972&plot=short&r=json')
-.then(appendMovie(2)).catch(errorLog(2));
+.then(function(data) {
+  $('#movie2 .picture').append('<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">');
+  $('#movie2 .text').append('<p><b>' + data.Title + '</b></p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
+})
+.catch(errorLog(2));
 
 $.get('http://www.omdbapi.com/?i=tt1399683&plot=short&r=json')
-.then(appendMovie(3)).catch(errorLog(3));
-
+.then(function(data) {
+  $('#movie3 .picture').append('<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">');
+  $('#movie3 .text').append('<p><b>' + data.Title + '</b></p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
+})
+.catch(errorLog(3));
 //function appendMovie(data, movieNum) {
 //  .then(function(data) {
     // $('#movie' + movieNum + ' .picture').append('<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">');
@@ -25,12 +39,3 @@ $.get('http://www.omdbapi.com/?i=tt1399683&plot=short&r=json')
   //   console.log('An error occurred with getting movie number ' + movieNum);
   // })
 //};
-
-function appendMovie(data, movieNum) {
-  $('#movie' + movieNum + ' .picture').append('<img src=\"' + data.Poster + '\" alt=\"' + data.Title + '\">');
-  $('#movie' + movieNum + ' .text').append('<p>' + data.Title + '</p><p> Year released: ' + data.Year + '</p><p>Rated ' + data.Rated + '</p><p>Plot: ' + data.Plot + '</p>');
-};
-
-function errorLog(movieNum) {
-  console.log('An error occurred with getting movie number ' + movieNum);
-};
